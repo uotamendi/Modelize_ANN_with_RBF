b360e9159f584ead13aea21ea885b639ee25395c
Urtzi Otamendi <uotamendi@vicomtech.org>
2020-01-16 15:06:00 +0100

    Merge main with RBF

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from modelDataManager import ModelDataManager
 from irisManager import IrisManager
-## The filename of the CSV where all the data created by the target ANN will be saved
-filename="irisTrainData.csv"
 
-## Initialize model's data manager
-dataManager=ModelDataManager(filename)
+from rbfManager import RbfManager
+get_data=False
+if get_data:
+    ## The filename of the CSV where all the data created by the target ANN will be saved
+    filename="irisTrainData.csv"
 
-## Initialize Iris model's manager
-irisManager=IrisManager()
+    ## Initialize model's data manager
+    dataManager=ModelDataManager(filename)
 
-## Load training data for 200 iterations
-try: 
-    for i in range(200):
-        print("----> Iteration Nº{}".format(i))
-        hp,loss,acc=irisManager.run()
-        dataManager.addRow(hp,loss,acc)
-except :
+    ## Initialize Iris model's manager
+    irisManager=IrisManager()
+
+    ## Load training data for 200 iterations
+    try: 
+        for i in range(200):
+            print("----> Iteration Nº{}".format(i))
+            hp,loss,acc=irisManager.run()
+            dataManager.addRow(hp,loss,acc)
+    except :
+        dataManager.save()
     dataManager.save()
-dataManager.save()
+
+else:
+    rbfManager=RbfManager()
+    rbfManager.run()
+    rbfManager.save()
